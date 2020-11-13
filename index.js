15b3f35134248a2f73375930e523d5f50cd77c63
natalie-poulson <natalieplsn@gmail.com>
2020-11-12 16:27:05 -0800

    figured out how to display info

--- index.js
+++ index.js
@@ -48,26 +48,20 @@ document.querySelector('form').addEventListener('change', () => {
     } 
 });
 
-const addInfoEffect = (idName, text) => {
-    document.querySelector(`#${idName}`).addEventListener('focus', () =>{
-        document.querySelector('.tooltip').style.display = 'inline-block';
-        document.querySelector('.tooltipText').style.visibility = 'visible';
-        document.querySelector('.tooltipText').style.opacity = 1;
-        document.querySelector('.tooltipText').innerHTML = text;
+const showInfo = category => {
+    document.querySelector(`#${category}`).addEventListener('focus', () =>{
+        document.querySelector(`.${category}Info`).style.display = 'block';
     });
 
-    document.querySelector(`#${idName}`).addEventListener('blur', () =>{
-        document.querySelector('.tooltip').style.display = 'none';
-        document.querySelector('.tooltipText').style.visibility = 'collapse';
-        document.querySelector('.tooltipText').style.opacity = 0;
-    })
-}
+    document.querySelector(`#${category}`).addEventListener('blur', () =>{
+        document.querySelector(`.${category}Info`).style.display = 'none';
+    });
+};
 
-addInfoEffect('income', '<span><b>Individual Income:</b> Consider all sources of income including, but not limited to: jobs, gigs, rental income, dividends, commissions, money you currently get from family members etc.</span>');
-addInfoEffect('rent', '<span><b>Housing:</b> Consider your current living situation, including whether you rent or own.</span>');
-addInfoEffect('debt', '<span><b>Debt:</b> Consider all student, medical, and consumer debt that you currently have. Do not include mortgage debt, this is accounted for in the housing category.</span>');
-addInfoEffect('wealth', '<span><b>Family Wealth:</b> Consider wealth that you currently have access to because of family or other relationships, as well as what you might inherit or owe when family members pass away. Wealth you have access to or may inherit includes: money, gifts, stocks, land and property, trusts, cars, etc. Negative family wealth means you don’t inherit anything of value and have responsibility to pay expenses that come up when a family member dies, like funeral, casket, legal fees, clergy, etc.</span>');
-addInfoEffect('edu', '<span><b>Level of Education:</b> Consider your current status. We acknowledge that not all advanced degrees are created equally—some have higher earning potential and social capital than others. We’ve tried to account for this and we also encourage you to think about your degree(s) in terms of social hierarchy (e.g. MDs and JDs often have higher earning potential than MFAs).</span>');
-addInfoEffect('dependents', '<span><b>Number of Dependents:</b> Consider the people who rely on you for financial support, including children, elders, or others.</span>');
-addInfoEffect('savings', '<span><b>Savings:</b> Consider the money that is in your savings account, retirement funds (401k, IRA, SEP, etc), investments in stocks, bonds, and mutual funds.</span>');
-
+showInfo('income');
+showInfo('rent');
+showInfo('debt');
+showInfo('wealth');
+showInfo('edu');
+showInfo('dependents');
+showInfo('savings');
